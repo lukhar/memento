3e381fec78359b19046dbc085615ea61e243a4bf
lukhar <lukasz.har@gmail.com>
2014-12-14 23:53:48 +0000

    Moved mongo dependent tests to integartion

--- build.py
+++ build.py
@@ -2,6 +2,7 @@ from pybuilder.core import use_plugin, init
 
 use_plugin('python.core')
 use_plugin('python.unittest')
+use_plugin('python.integrationtest')
 use_plugin('python.install_dependencies')
 use_plugin('python.flake8')
 use_plugin('python.distutils')
@@ -14,6 +15,8 @@ default_task = 'publish'
 @init
 def initialize(project):
     project.set_property('unittest_module_glob', 'test_*')
+    project.set_property('integrationtest_file_glob', 'test_*')
+
     project.build_depends_on('flask')
     project.build_depends_on('nose')
     project.build_depends_on('pyassert')
